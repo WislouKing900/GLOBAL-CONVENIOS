157299673f09909521eb6dd6b498fb687b3792f9
COLETA <COLETA@MOR442898-N.domhiae.einstein.br>
2025-05-07 12:51:22 -0300

    fazendo dados

---
+++ Dados.js
@@ -0,0 +1,19 @@
+let dados = [
+    {
+        titulo: 'Sulamerica',
+        descrição: 'executivo I',
+        tags: ''
+    },
+
+    {
+        titulo: 'Bradesco',
+        descrição: 'saude top nacional plus',
+        tags: ''
+    },
+
+    {
+        titulo: 'Omint',
+        descrição: 'medicina completo C20',
+        tags: ''
+    },
+]
